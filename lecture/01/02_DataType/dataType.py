be3ba48ec515838e68a3518f13bca2fff736e86c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-07 14:22:19 +0300

    add 1 lecture data type task

---
+++ lecture/01/02_DataType/dataType.py
@@ -0,0 +1,19 @@
+# типы данных и переменные
+# int, float, boolean, str, list, None
+
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(a)
+print(b)
+value = 12334
+print(value)
+
+print(type(value))
+
+s = 'hello world'
+print(s)
+print(a, b, s)
+print(a, '-', b, '-', s)
+print('{} - {} - {}'.format(a, b, s))
