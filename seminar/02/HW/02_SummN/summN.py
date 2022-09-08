2024ff94e40bf71b2fdb2157aaed2a3e76f379d7
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-08 17:05:42 +0300

    add HW 2 Task 2 Summ N

---
+++ seminar/02/HW/02_SummN/summN.py
@@ -0,0 +1,11 @@
+# Требуется посчитать сумму целых чисел, расположенных между числами 1 и N включительно.
+from os import system
+
+system('cls')
+
+n = int(input('Введите число N: '))
+summ = 0
+for i in range(1, n + 1):
+	summ += i
+
+print(f'Сумма равна: {summ}')
