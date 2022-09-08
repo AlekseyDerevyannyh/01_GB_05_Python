2e90368b08b4936c82e4fd1843ca12e6712c873b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-08 17:24:55 +0300

    add HW 2 Task 3 Min Divider

---
+++ seminar/02/HW/03_MinDivider/minDivider.py
@@ -0,0 +1,16 @@
+# Требуется найти наименьший натуральный делитель целого числа N, отличный от 1
+
+from os import system
+
+system('cls')
+
+n = int(input('Введите число N: '))
+if n < 2:
+	print('ОШИБКА! N должно быть больше 1!')
+else:
+	div = 0
+	for i in range(2, n + 1):
+		if n % i == 0:
+			div = i
+			break
+	print(f'Наименьший натуральный делитель {n} = {div}')
