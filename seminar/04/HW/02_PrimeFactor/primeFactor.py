4ea2b897ae5ae2ce6186756c88f57ddad8e72872
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-13 19:20:12 +0300

    add HW 4 Task 2 Prime Factor

---
+++ seminar/04/HW/02_PrimeFactor/primeFactor.py
@@ -0,0 +1,24 @@
+# Задайте натуральное число N. Напишите программу, которая составит
+# список простых множителей числа N.
+
+from os import system
+from tkinter import W
+
+system('cls')
+
+n = int(input('Введите число N: '))
+if n < 2:
+	print('ОШИБКА! Число N должно быть больше 1!')
+	exit()
+factor = []
+n
+i = 2
+while i <= n:
+	if n % i == 0:
+		n //= i
+		factor.append(i)
+		i = 2
+	else:
+		i += 1
+
+print(factor)
