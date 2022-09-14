cb40fd7462490a28e9cd25452849cef470359bf4
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-14 21:47:56 +0300

    add HW 4 Task 4 Polynom

---
+++ seminar/04/HW/04_Polynom/polynom.py
@@ -0,0 +1,51 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from os import system
+from random import randint
+
+system('cls')
+
+k = int(input('Введите число k: '))
+if k < 1:
+	print('ОШИБКА! Число k должно быть больше 0!')
+	exit()
+coefList = [randint(1, 100)]	# коэффициент старшего члена многочлена берём от 1
+for i in range(1, k + 1):
+	coefList.append(randint(0, 100))
+
+polynom = ''
+for i in range(len(coefList) - 1):
+	if coefList[i] == 0:
+		if coefList[i + 1] != 0:
+			polynom = polynom + ' + '
+	elif coefList[i] == 1:
+		if coefList[i + 1] != 0:
+			if len(coefList) - i - 1 == 1:
+				polynom = polynom + f'x + '
+			else:
+				polynom = polynom + f'x^{len(coefList) - i - 1} + '
+		else:
+			if len(coefList) - i - 1 == 1:
+				polynom = polynom + f'x'
+			else:
+				polynom = polynom + f'x^{len(coefList) - i - 1}'
+	else:
+		if coefList[i + 1] != 0:
+			if len(coefList) - i - 1 == 1:
+				polynom = polynom + f'{coefList[i]}*x + '
+			else:
+				polynom = polynom + f'{coefList[i]}*x^{len(coefList) - i - 1} + '
+		else:
+			if len(coefList) - i - 1 == 1:
+				polynom = polynom + f'{coefList[i]}*x'
+			else:
+				polynom = polynom + f'{coefList[i]}*x^{len(coefList) - i - 1}'
+
+if coefList[-1] != 0:
+	polynom = polynom + f'{coefList[-1]} = 0'
+else:
+	polynom = polynom + ' = 0'
+print(polynom)
