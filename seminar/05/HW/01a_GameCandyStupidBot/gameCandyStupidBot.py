08e0fb7d4d94ce4b67551765de7b08d789e13579
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-18 14:44:02 +0300

    add HW 5 Task 1a & 1b Game Candy with Stupid Bot & AI Bot

---
+++ seminar/05/HW/01a_GameCandyStupidBot/gameCandyStupidBot.py
@@ -0,0 +1,45 @@
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход
+# друг после друга. Первый ход определяется жеребьёвкой. За один ход можно
+# забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему
+# последний ход. Сколько конфет нужно взять первому игроку,
+# чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+
+from os import system
+from random import randint
+
+system('cls')
+
+candiesOnTable = 2021
+candiesLimit = 28
+candiesGamers = [0, 0]
+gamer = randint(0, 1)		# Жеребьёвка. 0 - ходит игрок. 1 - ходит бот
+if gamer:	print('Результат жеребьёвки: первым ходит бот')
+else:		print('Результат жеребьёвки: первым ходит игрок')
+winner = 0
+
+while candiesOnTable:
+	print('')
+	print('игрок	стол	бот')
+	print(f'{candiesGamers[0]}	{candiesOnTable}	{candiesGamers[1]}')
+	if gamer:
+		candies = randint(1, candiesLimit)
+		print(f'Бот забирает конфет:  {candies}')
+	else:
+		candies = int(input('Сколько конфет забирает игрок?: '))
+
+	if candies > candiesLimit:	candies = candiesLimit
+	elif candies < 0:			candies = 0
+
+	if candies >= candiesOnTable:
+		candiesOnTable = 0
+		winner = gamer
+	else:
+		candiesGamers[gamer] += candies
+		candiesOnTable -= candies
+		if gamer:	gamer = 0
+		else:		gamer = 1
+
+if winner:	print('Победил бот')
+else:		print('Победил игрок')
