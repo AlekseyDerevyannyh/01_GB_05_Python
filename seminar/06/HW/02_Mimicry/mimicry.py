d36a8e156188590be864386c611766a2f1cfd1cd
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-20 15:42:11 +0300

    add HW 6 Task 2 Mimicry

---
+++ seminar/06/HW/02_Mimicry/mimicry.py
@@ -0,0 +1,13 @@
+# Мимикрия
+
+from os import system
+
+system('cls')
+
+transformation = lambda x: x
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+	print('ok')
+else:
+	print('fail')
