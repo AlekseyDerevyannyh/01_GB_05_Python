35959db45c369a304c3eb61d6554ba64e7006805
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-24 00:14:20 +0300

    update HW 7 Phonebook

--- seminar/07/HW/Phonebook/read_pb.py
+++ seminar/07/HW/Phonebook/read_pb.py
@@ -1,9 +1,7 @@
 def read_phonebook ():
 	data = open('seminar/07/HW/Phonebook/phonebook.txt', 'r', encoding = 'UTF-8')
 
-	phones = []
-	for line in data:
-		phones.append(line[:-1])
+	phones = [line[:-1] for line in data]
 	phones.sort()
 	for contact in phones:
 		print(contact)
--- seminar/07/HW/Phonebook/search.py
+++ seminar/07/HW/Phonebook/search.py
@@ -1,13 +1,9 @@
 def search_contact (value):
 	data = open('seminar/07/HW/Phonebook/phonebook.txt', 'r', encoding = 'UTF-8')
 
-	phones = []
-	for line in data:
-		phones.append(line[:-1])
-	result = []
-	for contact in phones:
-		if value.lower() in contact.lower():
-			result.append(contact)
+	phones = [line[:-1] for line in data]
+	result = [contact for contact in phones if value.lower() in contact.lower()]
+
 	for contact in result:
 		print(contact)
 	data.close()
