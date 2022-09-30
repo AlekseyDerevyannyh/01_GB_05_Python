2f39aa8bf9294a9c0ea61ae4d02b20f92983d16b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-30 22:21:20 +0300

    add Seminar 9 Telegram Bot

---
+++ seminar/09_TelegramBot/main.py
@@ -0,0 +1,29 @@
+# pip install python-telegram-bot
+import secret
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from random import randint as rd
+
+bot = Bot(token = secret.key)
+updater = Updater(token = secret.key)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+	context.bot.send_message(update.effective_chat.id, 'Hello')
+
+def rand(update, context):
+	context.bot.send_message(update.effective_chat.id, f'{rd(1,100)}')
+
+def voice(update, context):
+	context.bot.send_message(update.effective_chat.id, 'Надоели Ваши голосовые сообщения!! Я пока что не научился их читать!')
+
+start_handler = CommandHandler('start', start)
+random_handler = CommandHandler('random', rand)
+message_handler = MessageHandler(Filters.voice, voice)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(random_handler)
+dispatcher.add_handler(message_handler)
+
+updater.start_polling()
+updater.idle()
