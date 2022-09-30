bc38ad59a51726b599b30d28f2f1c3ecc40b64fa
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-30 22:53:14 +0300

    update Seminar 9 add dialog with bot

--- seminar/09_TelegramBot/main.py
+++ seminar/09_TelegramBot/main.py
@@ -15,11 +15,15 @@ def rand(update, context):
 	context.bot.send_message(update.effective_chat.id, f'{rd(1,100)}')
 
 def voice(update, context):
-	context.bot.send_message(update.effective_chat.id, 'Я пока таких команд не знаю :(')
+	text = update.message.text
+	if 'прив' in text.lower():
+		context.bot.send_message(update.effective_chat.id, 'И тебе привет, мой друг!')
+	else:
+		context.bot.send_message(update.effective_chat.id, 'Я тебя не понимаю :(')
 
 start_handler = CommandHandler('start', start)
 random_handler = CommandHandler('random', rand)
-message_handler = MessageHandler(Filters.command, voice)
+message_handler = MessageHandler(Filters.text, voice)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(random_handler)
