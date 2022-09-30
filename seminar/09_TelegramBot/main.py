aff2bff40f981eb315f4f624eb4fee3caf2a4e76
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-30 23:34:02 +0300

    update Seminar 9 add dialog in bot

--- seminar/09_TelegramBot/main.py
+++ seminar/09_TelegramBot/main.py
@@ -2,7 +2,7 @@
 # pip install wikipedia
 import secret
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from random import randint as rd
 
 import wikipedia
@@ -12,36 +12,45 @@ bot = Bot(token = secret.key)
 updater = Updater(token = secret.key)
 dispatcher = updater.dispatcher
 
+A = 0
+B = 1
+
+
 def start(update, context):
-	context.bot.send_message(update.effective_chat.id, 'Hello')
+	context.bot.send_message(update.effective_chat.id, 'Привет\nКак твои дела?')
 
-def rand(update, context):
-	context.bot.send_message(update.effective_chat.id, f'{rd(1,100)}')
+	return A
 
-def wiki(update, context):
-	text = ' '.join(context.args)
-	try:
-		result = wikipedia.summary(text, sentences=2)
-		context.bot.send_message(update.effective_chat.id, result)
-	except:
-		context.bot.send_message(update.effective_chat.id, 'Не найдено!')
-
-def voice(update, context):
+def howareyou(update, context):
 	text = update.message.text
-	if 'прив' in text.lower():
-		context.bot.send_message(update.effective_chat.id, 'И тебе привет, мой друг!')
+	if 'хор' in text.lower():
+		context.bot.send_message(update.effective_chat.id, 'Я рад, что у тебя всё хорошо')
 	else:
-		context.bot.send_message(update.effective_chat.id, 'Я тебя не понимаю :(')
+		context.bot.send_message(update.effective_chat.id, 'Не грусти, всё будет отлично')
+	context.bot.send_message(update.effective_chat.id, 'Как погода?')
+
+	return B
+
+def weather(update, context):
+	text = update.message.text
+	context.bot.send_message(update.effective_chat.id, 'Ну ок, у меня тоже сегодня хорошая погода')
+
+	return ConversationHandler.END
+
+def cancel(update, context):
+	context.bot.send_message(update.effective_chat.id, 'Прощай!!!')
 
 start_handler = CommandHandler('start', start)
-random_handler = CommandHandler('random', rand)
-wiki_handler = CommandHandler('wiki', wiki)
-message_handler = MessageHandler(Filters.text, voice)
+howareyou_handler = MessageHandler(Filters.text, howareyou)
+weather_handler = MessageHandler(Filters.text, weather)
+cancel_handler = CommandHandler('cancel', cancel)
+conv_handler = ConversationHandler(entry_points=[start_handler],
+									states={A: [howareyou_handler],
+									B: [weather_handler]},
+									fallbacks=[cancel_handler])
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(random_handler)
-dispatcher.add_handler(wiki_handler)
-dispatcher.add_handler(message_handler)
+dispatcher.add_handler(cancel_handler)
+dispatcher.add_handler(conv_handler)
 
 updater.start_polling()
-updater.idle()
+updater.idle() # ctrl + c
